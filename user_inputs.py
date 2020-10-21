e97faa695fa7bce1a2fcaa4906efea05ae8593e1
Richard Kim <rk.sungjin@gmail.com>
2020-10-21 10:15:02 -1000

    Update user_inputs.py

--- user_inputs.py
+++ user_inputs.py
@@ -3,3 +3,14 @@ name = input("What is your name?\n")
 outputString = "Aloha " + name + "!"
 # OR print ("Aloha", name + "!")
 print (outputString)
+
+#Pay Calculator
+hoursString = input("Enter hours:")
+hours = float(hoursString)
+
+rateString = input("Enter rate:")
+rate = float(rateString)
+
+pay = hours * rate
+print("Pay:", pay)
+
