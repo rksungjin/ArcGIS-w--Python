52912061b537607355f0c629d8ed4168fbe0014c
Richard Kim <rk.sungjin@gmail.com>
2020-10-26 10:32:55 -1000

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,14 @@
+# Convert grade to letter
+
+scoreString = input("Enter score: ")
+score = int(scoreString)
+
+if score > 100 : 
+    print ("Bad Score")
+elif score >= 90 : 
+    print ("A")
+elif score >=80 : 
+    print ("B")
+elif score >=70 : 
+    print ("C")
+elif score 
