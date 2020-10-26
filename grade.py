5e0124a7c677c1ab534d2916b78fa8319a5220d4
Richard Kim <rk.sungjin@gmail.com>
2020-10-26 10:34:26 -1000

    Update grade.py

--- grade.py
+++ grade.py
@@ -3,7 +3,7 @@
 scoreString = input("Enter score: ")
 score = int(scoreString)
 
-if score > 100 : 
+if score > 100 or score < 0: 
     print ("Bad Score")
 elif score >= 90 : 
     print ("A")
@@ -11,4 +11,7 @@ elif score >=80 :
     print ("B")
 elif score >=70 : 
     print ("C")
-elif score 
+elif score >=60 :
+    print ("D")
+else: 
+    print("F")
