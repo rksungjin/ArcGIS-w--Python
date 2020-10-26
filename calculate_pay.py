c7f0580683da7ea1b5756c2498bb3e58053fe9f8
Richard Kim <rk.sungjin@gmail.com>
2020-10-26 10:07:20 -1000

    Update calculate_pay.py

--- calculate_pay.py
+++ calculate_pay.py
@@ -2,3 +2,12 @@
 hoursString = input("Enter hours: ")
 hours = float(hoursString)
 
+rateString = input("Enter rate: ")
+rate = float(rateString)
+
+if hours <= 40: 
+    pay = hours * rate
+else:
+    pay = 40 * rate + (hours - 40) * 1.5 * rate
+
+print("Pay: ", pay)
