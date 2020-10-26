1a909bf331594c8641752c07dca36ea0dfcdbf19
Richard Kim <rk.sungjin@gmail.com>
2020-10-26 10:29:53 -1000

    Update calculate_pay.py

--- calculate_pay.py
+++ calculate_pay.py
@@ -2,14 +2,22 @@
 try: 
     hoursString = input("Enter hours: ")
     hours = float(hoursString)
-
-    rateString = input("Enter rate: ")
-    rate = float(rateString)
-
-    if hours <= 40: 
-        pay = hours * rate
-    else:
-        pay = 40 * rate + (hours - 40) * 1.5 * rate
-    print("Pay: ", pay)
 except: 
     print("Error, please enter numberic input") 
+    exit(0)
+try:     
+    rateString = input("Enter rate: ")
+    rate = float(rateString)
+except: 
+    print("Error, please enter numberic input") 
+    exit(0)
+
+if hours <= 40: 
+    pay = hours * rate
+    print("Gross Pay: ", pay)
+else:
+    basepay = 40 * rate
+    overtime = (hours - 40) * 1.5 * rate
+    pay = basepay + overtime
+    #pay = 40 * rate + (hours - 40) * 1.5 * rate
+    print("Gross Pay: ", pay, "Base Pay: ", basepay, "Overtime Pay: ", overtime)
