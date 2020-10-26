931ac393dbe25138adfa660f983c1ca7e9de1879
Richard Kim <rk.sungjin@gmail.com>
2020-10-26 10:20:49 -1000

    update calculate

--- calculate_pay.py
+++ calculate_pay.py
@@ -1,13 +1,15 @@
 # Calculate pay
-hoursString = input("Enter hours: ")
-hours = float(hoursString)
+try: 
+    hoursString = input("Enter hours: ")
+    hours = float(hoursString)
 
-rateString = input("Enter rate: ")
-rate = float(rateString)
+    rateString = input("Enter rate: ")
+    rate = float(rateString)
 
-if hours <= 40: 
-    pay = hours * rate
-else:
-    pay = 40 * rate + (hours - 40) * 1.5 * rate
-
-print("Pay: ", pay) 
+    if hours <= 40: 
+        pay = hours * rate
+    else:
+        pay = 40 * rate + (hours - 40) * 1.5 * rate
+    print("Pay: ", pay)
+except: 
+    print("Error, please enter numberic input") 
