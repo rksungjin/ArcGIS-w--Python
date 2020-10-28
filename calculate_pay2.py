d7b3c142f50f50cb2d1d3a0fcaffc8e5a660add4
Richard Kim <rk.sungjin@gmail.com>
2020-10-28 10:11:23 -1000

    Update calculate_pay2.py

--- calculate_pay2.py
+++ calculate_pay2.py
@@ -6,3 +6,20 @@ def calculatePay (r, h) :
 
     else: 
         p = r * 40 + (h -40) * 1.5 * r
+        return p
+
+try: 
+    hoursString = input("Enter hours: ")
+    hours = float(hoursString)
+except: 
+    print("Error, please enter numberic input") 
+    exit(0)
+try:     
+    rateString = input("Enter rate: ")
+    rate = float(rateString)
+except: 
+    print("Error, please enter numberic input") 
+    exit(0)
+
+pay = calculatePay(rate, hours)
+print("Gross pay: ", pay)
