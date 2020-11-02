6a8a93f90d4911365cf265bc776136ce7e9045aa
Richard Kim <rk.sungjin@gmail.com>
2020-11-02 10:29:39 -1000

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,17 +1,21 @@
 #Guessing Game
 
 target = 42
+guesses = 7
 
-guessString = input("Guess my number: ")
-try: 
-    guess = int(guessString)
-except: 
-    print("Enter a number!")
-    exit()
-
-if guess < target: 
-    print("Higher!")
-if guess > target:
-    print("Lower!")
-else: 
-    print("You Win!")
+while guesses > 0:
+    guessString = input("Guess my number: ")
+    try: 
+        guess = int(guessString)
+    except: 
+        print("Enter a number!")
+        exit()
+        continue
+    guesses = guesses - 1
+    if guess < target: 
+        print("Higher!")
+    if guess > target:
+        print("Lower!")
+    else: 
+        print("You Win!")
+        break
