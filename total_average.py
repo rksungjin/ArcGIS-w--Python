4ed87bd22445c74b26457dccb712bd8fee77daf7
Richard Kim <rk.sungjin@gmail.com>
2020-11-02 10:15:40 -1000

    Update total_average.py

--- total_average.py
+++ total_average.py
@@ -2,6 +2,8 @@
 
 count = 0
 total = 0
+smallest = None
+largest = None
 
 while True: 
     valueString = input("Enter a number: ")
@@ -14,6 +16,10 @@ while True:
         continue
     count = count + 1
     total = total + value
+    if smallest == None or smallest > value : 
+        smallest = value
+    if largest == None or largest < value :
+        largest = value
 
 print("Total: ", total)
 print("Count: ", count)
@@ -23,3 +29,5 @@ if count == 0 :
     print("Average: 0")
 else :
     print("Average: ", total/count)
+print("Largest: ", largest)
+print("Smallest: ", smallest)
