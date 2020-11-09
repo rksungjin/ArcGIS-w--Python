a40f3fdb292d4b7e250cb31f0592f55c1bcf343b
Richard Kim <rk.sungjin@gmail.com>
2020-11-09 10:12:17 -1000

    Create store_text.py

---
+++ store_text.py
@@ -0,0 +1,20 @@
+
+fOut = open("info.txt", "w")
+
+print("Enter some text, 'done' to stop:")
+
+while True :
+    text = input()
+    if text == 'done':
+        break
+fOut.write(text + '\n')
+fOut.close()
+
+print("The new file contains:")
+
+fIn = open("info.txt")
+
+for line in fIn:
+    print(line)
+
+fIn.close()
