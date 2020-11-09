9140a291e5b0a5c51c9ce1f7eb1177a5eb389026
Richard Kim <rk.sungjin@gmail.com>
2020-11-09 10:54:20 -1000

    Update calculate_pay2.py

--- calculate_pay2.py
+++ calculate_pay2.py
@@ -8,24 +8,48 @@ def calculatePay (r, h) :
         p = r * 40 + (h -40) * 1.5 * r
         return p
 
+def getHours():
+    hoursString = input("Enter hours: ")
+    h = -1
+    try: 
+        h = float(hoursString)
+    except: 
+        print("Error, please enter numeric input for hours")
+    return h
+    
+def getRate():
+    rateString = input("Enter rate: ")
+    r = -1
+    try: 
+        r = float(rateString)
+    except: 
+        print("Error, please enter numeric input for rate")
+    return r
+
 fOut = open("payroll.csv", "w")
     
 while True:
     name = input("Enter Name: ")
     if name == 'done':
         break
-    try: 
-        hoursString = input("Enter hours: ")
-        hours = float(hoursString)
-    except: 
-        print("Error, please enter numberic input") 
-        continue
-    try:     
-        rateString = input("Enter rate: ")
-        rate = float(rateString)
-    except: 
-        print("Error, please enter numberic input") 
-        continue
+    hours = -1
+    while hours < 0: 
+        hours = getHours
+    rate = -1
+    while rate < 0:
+        rate = getRate()
+    #try: 
+        #hoursString = input("Enter hours: ")
+        #hours = float(hoursString)
+    #except: 
+        #print("Error, please enter numberic input") 
+        #continue
+    #try:     
+        #rateString = input("Enter rate: ")
+        #rate = float(rateString)
+    #except: 
+        #print("Error, please enter numberic input") 
+        #continue
 
     pay = calculatePay(rate, hours)
     outString = "%s, %.1f,%.2f,%.2f\n" % (name, hours, rate, pay)
