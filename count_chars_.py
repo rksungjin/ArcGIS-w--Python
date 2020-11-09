360953d0f89ef81b17de2a7f0fb0ce0a60868bb0
Richard Kim <rk.sungjin@gmail.com>
2020-11-09 10:28:38 -1000

    Update count_chars_.py

--- count_chars_.py
+++ count_chars_.py
@@ -8,3 +8,15 @@ outString = "The file contains %d characters" % (len(text))
 print(outString)
 
 fIn.close()
+
+fIn = open("info.txt")
+
+count = 0
+for line in fIn :
+    count = count + len(line)
+    print(len(line))
+
+outString = "The file contains %d characters" % (count)
+print(outString)
+
+fIn.close()
