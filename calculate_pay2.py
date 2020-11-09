0f82fdd2f91eae9ecf9a26ff3c27d1ae93c4be8e
Richard Kim <rk.sungjin@gmail.com>
2020-11-09 10:35:29 -1000

    Update calculate_pay2.py

--- calculate_pay2.py
+++ calculate_pay2.py
@@ -8,20 +8,29 @@ def calculatePay (r, h) :
         p = r * 40 + (h -40) * 1.5 * r
         return p
 
-try: 
-    hoursString = input("Enter hours: ")
-    hours = float(hoursString)
-except: 
-    print("Error, please enter numberic input") 
-    exit(0)
-try:     
-    rateString = input("Enter rate: ")
-    rate = float(rateString)
-except: 
-    print("Error, please enter numberic input") 
-    exit(0)
+fOut = open("payroll.csv", "w")
+    
+while True:
+    name = input("Enter Name: ")
+    if name == 'done':
+        break
+    try: 
+        hoursString = input("Enter hours: ")
+        hours = float(hoursString)
+    except: 
+        print("Error, please enter numberic input") 
+        continue
+    try:     
+        rateString = input("Enter rate: ")
+        rate = float(rateString)
+    except: 
+        print("Error, please enter numberic input") 
+        continue
 
-pay = calculatePay(rate, hours)
+    pay = calculatePay(rate, hours)
+    outString = "%s, %.1f,%.2f,%.2f\n" % (name, hours, rate, pay)
+    fOut.write(outString)
+    print(outString)
 #2 decimal places of accuracy
 #spaces/ field width then decimal place below
-print("Gross pay: %.2f" % (pay))
+    print("Gross pay: %.2f" % (pay))
