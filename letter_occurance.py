458d0eb9470155d890304d7d20fb866d87a2ee09
Richard Kim <rk.sungjin@gmail.com>
2020-11-16 11:02:44 -1000

    Create letter_occurance.py

---
+++ letter_occurance.py
@@ -0,0 +1,10 @@
+# Count occurances of letters
+
+letterCount = dict()
+text = 'extraordinary'
+for c in text:
+    if c not in letterCount:
+        letterCount[c] = 1
+    else:
+        letterCount[c] = letterCount[c] + 1
+print(letterCount)
