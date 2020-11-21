ba740a64df568aca5632320ffc69d0761acd8fca
Richard Kim <rk.sungjin@gmail.com>
2020-11-21 12:06:11 -1000

    Update count_letters.py

--- count_letters.py
+++ count_letters.py
@@ -1,12 +1,18 @@
 # Count the letters in a file
 
 fIn = open("thesonnets.txt")
+text = fIn.read()
+fIn.close()
 
 letterCounts = dict()
 
-for line in fIn:
-    letters = list(line)
-    for letter in letters:
-        letterCounts[letter] = letterCounts.get(letter, 0) + 1
+vowels = ['a', 'e', 'i', 'o', 'u']
 
+letters = list(text.lower())
+#for line in fIn:
+    #letters = list(line)
+for letter in letters:
+    if letter in vowels:
+        letterCounts[letter] = letterCounts.get(letter, 0) + 1
+counts = list()
 print(letterCounts)
